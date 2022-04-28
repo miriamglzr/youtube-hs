b6b6faeb25638ca22d174a752c56968801ea3fe8
miriam <miriam_glzr@hotmail.com>
2022-04-28 13:59:25 +0200

    getting videoId from params!

--- src/App.js
+++ src/App.js
@@ -1,47 +1,60 @@
-import {BrowserRouter as Router, Routes, Route, Outlet} from 'react-router-dom';
-import './App.css';
-import 'antd/dist/antd.css';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Outlet,
+} from "react-router-dom";
+import "./App.css";
+import "antd/dist/antd.css";
 
-import React, {useState, Suspense} from 'react';
+import React, { useState, Suspense } from "react";
 
-import {useVideo} from './context/selectedVideo';
+import { useVideo } from "./context/selectedVideo";
 
-import NavBar from './components/NavBar';
-import WatchPage from './components/WatchPage';
+import NavBar from "./components/NavBar";
+import WatchPage from "./components/WatchPage";
 
-const VideoListLazy = React.lazy (() => import ('./components/VideoList'));
-const VideoPlayerLazy = React.lazy (() => import ('./components/VideoPlayer'));
+const VideoListLazy = React.lazy(() => import("./components/VideoList"));
+const VideoPlayerLazy = React.lazy(() => import("./components/VideoPlayer"));
 
-function App () {
-  const [search, setSearch] = useState ('');
-  const [isSmall, setSmall] = useState (false);
-  const video = useVideo ();
-  return (
-    <Router className="App container">
-      <NavBar setSearch={setSearch} search={search}>
-
-        {video.id &&
-          <Suspense fallback={<div>Loading...</div>}>
-            <VideoPlayerLazy isSmall={isSmall} setSmall={setSmall} />
-          </Suspense>}
-        <Routes>
-          <Route path="/" element={<div> <Outlet /></div>} />
-          <Route
-            path=":search"
-            element={
-              <Suspense fallback={<div>Loading...</div>}>
-                <VideoListLazy isSmall={isSmall} setSmall={setSmall} />
-              </Suspense>
-            }
-          />
-          <Route
-            path="watch/:params"
-            element={<WatchPage isSmall={isSmall} />}
-          />
-        </Routes>
-      </NavBar>
-    </Router>
-  );
+function App() {
+	const [search, setSearch] = useState("");
+	const [isSmall, setSmall] = useState(false);
+	const video = useVideo();
+	return (
+		<Router className="App container">
+			<NavBar setSearch={setSearch} search={search}>
+				{video.id && (
+					<Suspense fallback={<div>Loading...</div>}>
+						<VideoPlayerLazy isSmall={isSmall} setSmall={setSmall} />
+					</Suspense>
+				)}
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<div>
+								{" "}
+								<Outlet />
+							</div>
+						}
+					/>
+					<Route
+						path=":search"
+						element={
+							<Suspense fallback={<div>Loading...</div>}>
+								<VideoListLazy isSmall={isSmall} setSmall={setSmall} />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="watch/:videoId"
+						element={<WatchPage isSmall={isSmall} />}
+					/>
+				</Routes>
+			</NavBar>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,31 +1,35 @@
-import {LikeOutlined} from '@ant-design/icons';
-import React from 'react';
+import { LikeOutlined } from "@ant-design/icons";
+import React from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useVideo } from "../context/selectedVideo";
 
-import {useVideo} from '../context/selectedVideo';
+export default function WatchPage() {
+	const { videoId } = useParams();
+	const video = useVideo();
+	const { id, selectedVideo, setId } = video;
 
-export default function WatchPage () {
-  // let [searchParams, setSearchParams] = useSearchParams ();
-  const video = useVideo ();
-  const {id, selectedVideo} = video;
-  //console.log (selectedVideo);
+	//get videoId from params
+	useEffect(() => {
+		setId(videoId);
+	}, [videoId]);
 
-  return (
-    selectedVideo &&
-    id &&
-    <div className="container mt-2">
-      <h4>{selectedVideo.title}</h4>
-      <p>
-        {selectedVideo.views}
-        {'    '}
-        views |
-        {'    '}
-        <LikeOutlined
-          style={{fontSize: '16px', cursor: 'pointer', marginRight: '5px'}}
-        />
-        {(Number (selectedVideo.views) * 0.009).toFixed (0)}
-      </p>
-      <p>{selectedVideo.description}</p>
-
-    </div>
-  );
+	return (
+		selectedVideo &&
+		id && (
+			<div className="container mt-2">
+				<h4>{selectedVideo.title}</h4>
+				<p>
+					{selectedVideo.views}
+					{"    "}
+					views |{"    "}
+					<LikeOutlined
+						style={{ fontSize: "16px", cursor: "pointer", marginRight: "5px" }}
+					/>
+					{(Number(selectedVideo.views) * 0.009).toFixed(0)}
+				</p>
+				<p>{selectedVideo.description}</p>
+			</div>
+		)
+	);
 }
