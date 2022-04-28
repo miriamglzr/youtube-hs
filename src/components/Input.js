ebf7c9e33745c0feb2e8a1f965df0b343ee81321
miriam <miriam_glzr@hotmail.com>
2022-04-28 18:34:27 +0200

    removed comments

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,39 +1,25 @@
-import {Input, Space} from 'antd';
+import { Input, Space } from "antd";
+import { Outlet, useNavigate } from "react-router-dom";
 
-import {Outlet, useNavigate} from 'react-router-dom';
-//import {useVideo} from '../context/selectedVideo';
+const { Search } = Input;
 
-const {Search} = Input;
+export default function SearchInput({ setSearch }) {
+	const navigate = useNavigate();
 
-// const suffix = (
-//   <AudioOutlined
-//     style={{
-//       fontSize: 16,
-//       color: 'rgba(0, 0, 0, 0.45)',
-//     }}
-//   />
-// );
+	const onSearch = (value) => {
+		setSearch(value);
+		value !== "" && navigate(`/${value}`);
+	};
 
-export default function SearchInput({setSearch}) {
-  const navigate = useNavigate ();
-  // const video = useVideo ();
-  const onSearch = value => {
-    //  console.log (value);
-    setSearch (value);
-
-    value !== '' && navigate (`/${value}`);
-  };
-
-  return (
-    <Space direction="vertical">
-
-      <Search
-        placeholder="Search"
-        allowClear
-        onSearch={onSearch}
-        style={{width: '50vw'}}
-      />
-      <Outlet />
-    </Space>
-  );
+	return (
+		<Space direction="vertical">
+			<Search
+				placeholder="Search"
+				allowClear
+				onSearch={onSearch}
+				style={{ width: "50vw" }}
+			/>
+			<Outlet />
+		</Space>
+	);
 }
